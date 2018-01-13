0935c140ff968cd4b4a4469f94de9e6e3adb50cf
cheeseconnoisseur <arturo.dezon@gmail.com>
2018-01-13 17:16:45 +0000

    added google calander it can now tell next events, requires client json file (not included for obvious reasons)

--- bigman.py
+++ bigman.py
@@ -2,6 +2,7 @@ from telegram.ext import Updater, CommandHandler, MessageHandler, Filters, Conve
 import logging
 import telegram
 from pytube import YouTube
+from calendarrr import maincal
 import os
 import random
 
@@ -9,7 +10,7 @@ import random
 logging.basicConfig(format='%(asctime)s - %(name)s - %(levelname)s - %(message)s',level=logging.INFO)
 logger = logging.getLogger(__name__)
 
-BIO = range(1)
+BIO, HM = range(2)
 
 def start(bot, update):
     user = update.message.from_user
@@ -39,12 +40,19 @@ def location(bot, update):
     stream.download()
     oldtitle = title
     bot.send_message(chat_id=update.message.chat_id,message_id=123, text="uploading")
+
+    #title = title + ".mp4"
+    #title = title.replace(":","")
+    #title = title.replace("?","")
+    ##YouTube(update.message.text).streams.first().download('media')
+    #bot.send_document(chat_id=update.message.chat_id, document=open(title, 'rb'),timeout=600)
+
     try:
         title = title + ".mp4"
         title = title.replace(":","")
         title = title.replace("?","")
         #YouTube(update.message.text).streams.first().download('media')
-        bot.send_document(chat_id=update.message.chat_id, document=open(title, 'rb'),timeout=400)
+        bot.send_document(chat_id=update.message.chat_id, document=open(title, 'rb'),timeout=600)
     except:
         print("lolno")
     try:
@@ -52,7 +60,7 @@ def location(bot, update):
         oldtitle = oldtitle.replace(":","")
         oldtitle = oldtitle.replace("?","")
         #YouTube(update.message.text).streams.first().download('media')
-        bot.send_document(chat_id=update.message.chat_id, document=open(oldtitle, 'rb'),timeout=400)
+        bot.send_document(chat_id=update.message.chat_id, document=open(oldtitle, 'rb'),timeout=600)
 
     except:
         print("lolno")
@@ -60,7 +68,10 @@ def location(bot, update):
         os.remove(title)
     except:
         print("no1")
+    try:
         os.remove(oldtitle)
+    except:
+        print("no2")
 
     print("lol")
     return (update.message.text)
@@ -72,9 +83,39 @@ def location(bot, update):
 #	text = update.message.text
 #	caption = update.message.caption
 
+def cal(bot, update):
+    update.message.reply_text('how many events?')
+    return HM
+
+def calget(bot, update):
+    user = update.message.text
+    bot.sendChatAction(chat_id=update.message.chat_id, action=telegram.ChatAction.TYPING, timeout = 600)
+    logger.info("starting")
+    maincal(int(user))
+    kay = []
+    with open('temp.txt','r') as f:
+        kay = f.read()
+        kay = kay.split("\n")
+        print(kay)
+    for event in kay:
+        print(kay)
+        event = str(event)
+        bot.send_message(chat_id=update.message.chat_id,message_id=123, text= event)
 
 
-
+def calnext(bot, update):
+    bot.sendChatAction(chat_id=update.message.chat_id, action=telegram.ChatAction.TYPING, timeout = 600)
+    logger.info("starting")
+    maincal(1)
+    kay = []
+    with open('temp.txt','r') as f:
+        kay = f.read()
+        kay = kay.split("\n")
+        print(kay)
+    for event in kay:
+        print(kay)
+        event = str(event)
+        bot.send_message(chat_id=update.message.chat_id,message_id=123, text= event)
 
 
 def ugan(bot, update):
@@ -124,10 +165,23 @@ def main():
         fallbacks=[CommandHandler("uganda", ugan)]
     )
 
+    conv_handler2 = ConversationHandler(
+        entry_points=[CommandHandler("cal", cal)],
+
+        states={
+
+            HM: [MessageHandler(Filters.text, calget)]
+        },
+
+        fallbacks=[CommandHandler("uganda", ugan)]
+    )
     dp.add_handler(conv_handler)
+    dp.add_handler(conv_handler2)
     dp.add_handler(CommandHandler("start", start))
     #dp.add_handler(MessageHandler(Filters.text, location))
     dp.add_handler(CommandHandler("uganda", ugan))
+    #dp.add_handler(CommandHandler("cal", cal))
+    dp.add_handler(CommandHandler("calnext", calnext))
     dp.add_handler(CommandHandler("logan", logan))
     dp.add_handler(CommandHandler("tide", tide))
     unknown_handler = MessageHandler(Filters.command, unknown, pass_chat_data=True)
